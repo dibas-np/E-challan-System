d7dab9fbca1b461b1ee1faa3823a571356b0623f
dibas-np <np03s200002@heraldcollege.edu.np>
2021-04-10 08:15:58 +0545

    fixing issue #7

--- js/AdminLogin.js
+++ js/AdminLogin.js
@@ -1,21 +1,31 @@
-
+//Admin Login Page
 function logIn() {
 	var username = document.getElementById('username').value
 	var password = document.getElementById('password').value
 
-	
-		// check is user input matches username and password of a current index of the objPeople array
+		// check is user input matches username and password
 		if(username == "admin" && password == "admin") {
 			console.log(username + " is logged in!!!")
-			// stop the function if this is found to be true
+			// redirects to Admin Page if true
             window.location.href = "../html/AdminPage.html"
 			return
 		}
-        swal.fire({
+    
+    //checks if username or password is empty and display appropriate message
+    if(username.trim() ==="" || password.trim() ===""){
+		swal.fire({
+            icon: 'error',
+            title: 'Error...',
+            text: 'Username or Password can\'t be empty!'
+          })
+	}else{
+		swal.fire({
             icon: 'error',
             title: 'Error...',
             text: 'Incorrect Username or Password!'
           })
-	console.log("incorrect username or password")
-    document.getElementById("login-form").reset();
+		console.log("incorrect username or password")
+        document.getElementById("login-form").reset();
+	}
+	
 }
