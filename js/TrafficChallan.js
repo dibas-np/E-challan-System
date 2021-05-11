367cdf21f101392bf772e3a5bd0730797976b89d
Rabins31 <np03s200004@heraldcollege.edu.np>
2021-05-12 00:05:33 +0545

    Solved Issue #11 Challan Page Buttons

--- js/TrafficChallan.js
+++ js/TrafficChallan.js
@@ -15,7 +15,7 @@ class TrafficChallanDetails{ //Class TrafficChallanDetails
     
       //Method to make data of admission ready through this variables
       makeDataChallan(){
-        let dataAdmission = {
+        let dataTrafficChallan = {
           fullName : this.fullName,
           address : this.address,
           licenceNo : this.licenceNo,
@@ -27,16 +27,16 @@ class TrafficChallanDetails{ //Class TrafficChallanDetails
           fineAmount : this.fineAmount
         };
     
-        //returns all values as dataAdmission
-        return dataAdmission;
+        //returns all values as dataTrafficChallan
+        return dataTrafficChallan;
       }
       //Method : Functions to post user input data in JSON Server
       trafficChallanDetail(){
         const data = this.makeDataChallan(); 
         //Use of axios.post
-        axios.post('http://localhost:3000/NewAdmission', data) //using 3000 as default port
+        axios.post('http://localhost:3000/ChallanDetails', data) //using 3000 as default port
         .then(()=>{
-            console.log("New Admission Request Recorded."); //Display output in console if submission is successful
+            console.log("New Challan Recorded."); //Display output in console if submission is successful
           }).catch((errorStore)=>{ //to Catch errors, if error occurs
             console.log(errorStore);
           });
@@ -45,19 +45,19 @@ class TrafficChallanDetails{ //Class TrafficChallanDetails
     }
     
     //admission form 
-    let admissionForm = document.querySelector('.admission-form');
-    admissionForm.addEventListener('submit', (eventObj)=>{
+    let challanForm = document.querySelector('.challan-form');
+    challanForm.addEventListener('submit', (eventObj)=>{
       eventObj.preventDefault(); //Prevents default behaviour
       // Admission Form variables(values) declaration
       let fullName = document.querySelector('#fullName').value;
       let address = document.querySelector('#address').value;
-      let licenceNo = document.querySelector('#licenceNo').value;
-      let vehicleCat = document.querySelector('#date').value;
-      let vehicleNum = document.querySelector('#level').value;
-      let createdBy = document.querySelector('.full-name').value;
-      let challanDate = document.querySelector('#challanDate').value;
-      let address = document.querySelector('#address').value;
-      let fineAmount = document.querySelector('#fineAmount').value;
+      let licenceNo = document.querySelector('#licenseno').value;
+      let vehicleCat = document.querySelector('#vehicle').value;
+      let vehicleNum = document.querySelector('#vehicleno').value;
+      let createdBy = document.querySelector('#creater').value;
+      let challanDate = document.querySelector('#date').value;
+      let guilty = document.querySelector('#problem').value;
+      let fineAmount = document.querySelector('#amount').value;
       
       //Making new object of the class TrafficChallanDetails
       let trafficChallanDetail = new TrafficChallanDetails(fullName, address, licenceNo, vehicleCat, vehicleNum, createdBy, challanDate, address, fineAmount);
@@ -65,7 +65,15 @@ class TrafficChallanDetails{ //Class TrafficChallanDetails
     
       eventObj.target.reset(); //Resets this form after every submission 
     });
+
+
+
+//To print challan 
+    function myPrint(challan) {
+        var printdata = document.querySelector(".challan-detail");
+        newwin = window.open("");
+        newwin.document.write(printdata.outerHTML);
+        newwin.print();
+        newwin.close();
+    }
     //end of the code
-    
-    
-    
