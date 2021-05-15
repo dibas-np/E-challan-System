bf2784bcce364a2776e20c6b11318b47d018d2e8
Hrishave <np03s200026@heraldcollege.edu.np>
2021-05-15 19:44:38 +0545

    Update of the code

--- js/userlogin.js
+++ js/userlogin.js
@@ -1,10 +1,5 @@
-//class Signin{
-  //  constructor(uname,password){
-    //    super(uname, password);     
-    //}
-    let butn = document.querySelector('#sub-btn');
-    let from = document.querySelector('#signin');
-//var registerDateArr = [];
+ let butn = document.querySelector('#sub-btn');
+ let from = document.querySelector('#signin');
 
 class Signin {
   constructor(uname, password){
@@ -12,28 +7,16 @@ class Signin {
     this.password = password;
      
     }
-    //method to format the data to store in json server
-   // creatingData(){
-      //  let data= {
-        //    uname: this.uname,
-          //  email: this.email,
-            //phonenumber: this.phonenumber,
-            //address: this.address,
-            //password: this.password,
-        //};
-        //return data;
-    //}
-   
-   //method to login to payment
+  
    userLogin(){
-    // get value from json server through locolhost 3000
+    // get value from json server locolhost 3000
     axios.get('http://localhost:3000/register')
     .then(res => {
-      // to check if username and password match from data value
+      // if username and password are coorect or not
       let currentUsers = res.data.filter(item=>{
         return (item.uname == this.uname && item.password == this.password);
       });
-     // function checkLoginDetails(args){
+
         if(currentUsers.length > 0){
             return true;
         }else{
@@ -44,15 +27,15 @@ class Signin {
     })
     .then(r =>{
         if(r == true){
-          swal.fire({ //swal is SweetAlert in JavaScript
-              icon: 'success', //success icon for display message
-              title: 'Login Success', //title of pop-up message
+          swal.fire({ 
+              icon: 'success', 
+              title: 'Login Success', 
             })
           window.location = "../html/UserPayment.html";
         }else{
-          swal.fire({ //swal is SweetAlert in JavaScript
-              icon: 'error', //icon for display message
-              title: 'Login Failed', //title of pop-up message
+          swal.fire({ 
+              icon: 'error', 
+              title: 'Login Failed', 
               text: 'Username or Password did not Match! Please try again with correct Username and Password.'
             })
         }
@@ -60,18 +43,18 @@ class Signin {
   }
 }
 
-//constant variable for login form
+
 const login = document.querySelector('#signin');
 if(login != null){
   // On clicking login button 
   login.addEventListener('submit', e => {
     e.preventDefault();
-    let uname = document.querySelector('#username').value; //get username value
-    let password = document.querySelector('#passw').value; //get password value
+    let uname = document.querySelector('#username').value; //get username 
+    let password = document.querySelector('#passw').value; //get password 
 
-    let userLogin = new Signin(uname, password); //class TrafficLogin()
+    let userLogin = new Signin(uname, password); 
     userLogin.userLogin();
   })
 }
-// End of code
+
 
